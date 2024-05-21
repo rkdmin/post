668e44bd3b16b7af70d3e0a266368951f7b68912
LeeKangMin <22rkdmin@thinkfree.com>
2024-05-21 16:29:53 +0900

    H2 DB 연결

--- src/main/java/com/postproject/post/PostController.java
+++ src/main/java/com/postproject/post/PostController.java
@@ -2,19 +2,19 @@ package com.postproject.post;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/post")
+@RequestMapping("/posts")
 public class PostController {
     private final PostService postService;
 
-    @GetMapping
-    public ResponseEntity<PostDto> createPost(@RequestBody PostDto request) {
+    @PostMapping
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto request) throws Exception {
         return ResponseEntity.ok(postService.create(request));
     }
 
--- src/main/java/com/postproject/post/PostService.java
+++ src/main/java/com/postproject/post/PostService.java
@@ -1,6 +1,7 @@
 package com.postproject.post;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,9 +9,16 @@ import org.springframework.stereotype.Service;
 public class PostService {
     private final PostRepository postRepository;
 
-    public PostDto create(PostDto request) {
+    public PostDto create(PostDto request) throws Exception {
+        // 제목이나 내용이 없으면 오류
+        if (Strings.isBlank(request.getTitle()) || Strings.isBlank(request.getContent())) {
+            throw new Exception("제목이나 내용이 없습니다.");
+        }
+
+        // 엔티티로 변경
         Post post = Post.toEntity(request);
 
+        // DB에 저장
         return PostDto.toDto(postRepository.save(post));
     }
 }
